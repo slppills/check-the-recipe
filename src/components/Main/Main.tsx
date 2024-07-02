0d93b1c36cc3f326e6e1e61c9f1b884ab21a0e6c
slppills <sunchaehoon2@naver.com>
2024-07-02 23:15:02 +0900

    perf main

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -17,27 +17,27 @@ const Main = () => {
 				<S.ContentTitle>Select menu type</S.ContentTitle>
 				<S.MenuContainer>
 					<S.SelectMenu onClick={() => navigate("/rice")}>
-						<S.MenuImg $url="/img/main/rice.svg" />
+						<S.MenuImg $url="/check-the-recipe/img/main/rice.svg" />
 						<span>밥</span>
 					</S.SelectMenu>
 					<S.SelectMenu onClick={() => navigate("/soup")}>
-						<S.MenuImg $url="/img/main/soup.svg" />
+						<S.MenuImg $url="/check-the-recipe/img/main/soup.svg" />
 						<span>국&찌개</span>
 					</S.SelectMenu>
 					<S.SelectMenu onClick={() => navigate("/sidedish")}>
-						<S.MenuImg $url="/img/main/sidedish.svg" />
+						<S.MenuImg $url="/check-the-recipe/img/main/sidedish.svg" />
 						<span>반찬</span>
 					</S.SelectMenu>
 					<S.SelectMenu onClick={() => navigate("/onedish")}>
-						<S.MenuImg $url="/img/main/onedish.svg" />
+						<S.MenuImg $url="/check-the-recipe/img/main/onedish.svg" />
 						<span>일품</span>
 					</S.SelectMenu>
 					<S.SelectMenu onClick={() => navigate("/dessert")}>
-						<S.MenuImg $url="/img/main/dessert.svg" />
+						<S.MenuImg $url="/check-the-recipe/img/main/dessert.svg" />
 						<span>후식</span>
 					</S.SelectMenu>
 					<S.SelectMenu onClick={() => navigate("/etc")}>
-						<S.MenuImg $url="/img/main/etc.svg" />
+						<S.MenuImg $url="/check-the-recipe/img/main/etc.svg" />
 						<span>기타</span>
 					</S.SelectMenu>
 				</S.MenuContainer>
