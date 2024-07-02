bc41d92808be129c3ea4952d2111be0863c31452
slppills <sunchaehoon2@naver.com>
2024-07-02 23:14:52 +0900

    perf router

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,14 +1,14 @@
 import { Route, Routes } from "react-router-dom";
 import Main from "./components/Main/Main";
-import Rice from "./components/Menu/Menu";
 import Dish from "./components/Dish/Dish";
 import Detail from "./components/Detail/Detail";
+import Menu from "./components/Menu/Menu";
 
 const Router = () => {
 	return (
 		<Routes>
 			<Route path="/" element={<Main />} />
-			<Route path="/:menuType" element={<Rice />} />
+			<Route path="/:menuType" element={<Menu />} />
 			<Route path="/:menuType/:makingType" element={<Dish />} />
 			<Route path="/:menuType/:makingType/:id" element={<Detail />} />
 		</Routes>
