140d0c1f62aabeb9f02afeddc9ff9c40e7570a12
slppills <sunchaehoon2@naver.com>
2024-07-06 15:27:36 +0900

    perf globalstyle

--- src/GlobalStyle.tsx
+++ src/GlobalStyle.tsx
@@ -6,32 +6,32 @@ const GlobalStyle = createGlobalStyle`
     padding: 0;
     box-sizing: border-box;
     font-family: 'TTLaundryGothicB';
-    
-    @media screen and (max-width: 2100px) {
-        font-size: 14px;
-	}
-    @media screen and (max-width: 1950px) {
+
+    @media screen and (max-width: 2100px), screen and (max-height: 1400px) {
+    font-size: 14px;
+    }
+    @media screen and (max-width: 1950px), screen and (max-height: 1300px) {
         font-size: 13px;
-	}
-    @media screen and (max-width: 1800px) {
+    }
+    @media screen and (max-width: 1800px), screen and (max-height: 1200px) {
         font-size: 12px;
-	}
-    @media screen and (max-width: 1650px) {
+    }
+    @media screen and (max-width: 1650px), screen and (max-height: 1100px) {
         font-size: 11px;
-	}
-    @media screen and (max-width: 1500px) {
+    }
+    @media screen and (max-width: 1500px), screen and (max-height: 1000px) {
         font-size: 10px;
     }
-    @media screen and (max-width: 1100px) {
+    @media screen and (max-width: 1100px), screen and (max-height: 900px) {
         font-size: 9px;
-	}
-    @media screen and (max-width: 1200px) {
+    }
+    @media screen and (max-width: 1200px), screen and (max-height: 800px) {
         font-size: 8px;
     }
-    @media screen and (max-width: 1050px) {
+    @media screen and (max-width: 1050px), screen and (max-height: 700px) {
         font-size: 7px;
     }
-    @media screen and (max-width: 900px) {
+    @media screen and (max-width: 900px), screen and (max-height: 600px) {
         font-size: 6px;
     }
 }
