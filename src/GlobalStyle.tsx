c9986f76234233150e2ce9efde391b1a03d4c570
slppills <sunchaehoon2@naver.com>
2024-07-06 15:39:49 +0900

    perf globalstyle

--- src/GlobalStyle.tsx
+++ src/GlobalStyle.tsx
@@ -7,31 +7,34 @@ const GlobalStyle = createGlobalStyle`
     box-sizing: border-box;
     font-family: 'TTLaundryGothicB';
 
-    @media screen and (max-width: 2100px), screen and (max-height: 1400px) {
-    font-size: 14px;
+    @media screen and (max-width: 2250px), screen and (max-height: 1265.625px) {
+        font-size: 15px;
     }
-    @media screen and (max-width: 1950px), screen and (max-height: 1300px) {
+    @media screen and (max-width: 2100px), screen and (max-height: 1181.25px) {
+        font-size: 14px;
+    }
+    @media screen and (max-width: 1950px), screen and (max-height: 1096.875px) {
         font-size: 13px;
     }
-    @media screen and (max-width: 1800px), screen and (max-height: 1200px) {
+    @media screen and (max-width: 1800px), screen and (max-height: 1012.5px) {
         font-size: 12px;
     }
-    @media screen and (max-width: 1650px), screen and (max-height: 1100px) {
+    @media screen and (max-width: 1650px), screen and (max-height: 928.125px) {
         font-size: 11px;
     }
-    @media screen and (max-width: 1500px), screen and (max-height: 1000px) {
+    @media screen and (max-width: 1500px), screen and (max-height: 843.75px) {
         font-size: 10px;
     }
-    @media screen and (max-width: 1100px), screen and (max-height: 900px) {
+    @media screen and (max-width: 1350px), screen and (max-height: 759.375px) {
         font-size: 9px;
     }
-    @media screen and (max-width: 1200px), screen and (max-height: 800px) {
+    @media screen and (max-width: 1200px), screen and (max-height: 675px) {
         font-size: 8px;
     }
-    @media screen and (max-width: 1050px), screen and (max-height: 700px) {
+    @media screen and (max-width: 1050px), screen and (max-height: 590.625px) {
         font-size: 7px;
     }
-    @media screen and (max-width: 900px), screen and (max-height: 600px) {
+    @media screen and (max-width: 900px), screen and (max-height: 506.25px) {
         font-size: 6px;
     }
 }
